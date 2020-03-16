d72461b5206fb7ad25aa66d00700bb52bcb2ecfb
stanley akyea <stanley.akyea@gmail.com>
2020-03-16 21:41:51 +0100

    Create homepage, directory and menu item components and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './components/pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      Hello Bolad!
+      <HomePage />
     </div>
   );
 }
