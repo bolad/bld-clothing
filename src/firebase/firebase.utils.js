e33c5ef8b6f9b361b79f19f79cd28ce9772b43ca
stanley akyea <stanley.akyea@gmail.com>
2020-03-20 21:34:02 +0100

    Store user info in firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,40 @@ const config = {
     appId: "1:76192001705:web:9cdb1ad71fd706280903b8",
     measurementId: "G-WQR7SMQQBJ"
   };
+
+  //take the userAuth object received from authentication library and store in database
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    //Get the user reference from firestore
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    //wait to GET the data from userRef and store it in snapShot
+    const snapShot = await userRef.get();
+
+    //Save snapShot to database if it does not exist
+    if(!snapShot.exists) {
+        //Destructure which data from userAuth you want to save to database
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        //Make asynchronous to the database to store the data
+        try {
+            //Create user with .set()
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+            
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+  }
   // Initialize Firebase
   firebase.initializeApp(config);
 
