a9d00df138a84c09c44b7a7c97e04c422a150e2d
stanley akyea <stanley.akyea@gmail.com>
2020-03-21 18:45:39 +0100

    Setup redux and create userReducer and rootReducer

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+    currenctUser: null
+}
+
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch(action.type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: action.payload
+            }
+        default:
+            return state;
+        end
+    }
+}
+
+export default userReducer;
