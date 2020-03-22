b383b0c292699871c355132e6b8838ebd7992e7d
stanley akyea <stanley.akyea@gmail.com>
2020-03-22 17:11:07 +0100

    Add card item component and logo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import { auth } from '../../firebase/firebase.utils';
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-
+import CartIcon from '../cart-icon/cart-icon.component';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
@@ -29,6 +27,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
