5da0b15e8416a02976349ad3d7f35948610a92f8
stanley akyea <stanley.akyea@gmail.com>
2020-03-22 22:42:34 +0100

    Implement Add to cart button in collection items and style

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 //pull the children off the props that get passed in the CustomButton and destructure all the other props
-//conditionally render the class 'google-sign-in' if isGoogleSignIn prop is true
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
-         {children}
+//conditionally render the class 'google-sign-in' if isGoogleSignIn prop is true, etc.
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <button 
+        className={`${inverted ? 'inverted' : '' }
+            ${isGoogleSignIn ? 'google-sign-in' : ''} 
+            custom-button`} {...otherProps}
+    >
+        {children}
     </button>
 );
 
