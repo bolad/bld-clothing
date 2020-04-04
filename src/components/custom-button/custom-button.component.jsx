a332a53b1efd6e36a00549723e0d4f3d51386d7f
stanley akyea <stanley.akyea@gmail.com>
2020-04-04 13:55:09 +0200

    Convert Custom button styles to javascript styled components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import './custom-button.styles.scss';
+
+import { CustomButtonContainer } from './custom-button.styles';
 
 //pull the children off the props that get passed in the CustomButton and destructure all the other props
 //conditionally render the class 'google-sign-in' if isGoogleSignIn prop is true, etc.
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps}) => (
-    <button 
-        className={`${inverted ? 'inverted' : '' }
-            ${isGoogleSignIn ? 'google-sign-in' : ''} 
-            custom-button`} {...otherProps}
-    >
-        {children}
-    </button>
+const CustomButton = ({children, ...props}) => (
+   <CustomButtonContainer {...props}>
+       {children}
+   </CustomButtonContainer>
+    
 );
 
 export default CustomButton;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/penguin.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { createStructuredSelector } from 'reselect';
