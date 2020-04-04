e217c525a3d39a296ec9257de5725a615a3954aa
stanley akyea <stanley.akyea@gmail.com>
2020-04-04 12:18:04 +0200

    Convert header and HomePage components styling to javascript styled components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 import Directory from '../../components/directory/directory.component';
 
-const HomePage = () => {
-    return (
-        <div className="homepage">
-            <Directory />
-        </div>
-    )
-}
+const HomePage = () => (
+    <HomePageContainer>
+         <Directory />
+    </HomePageContainer>
+)
 
 export default HomePage;
